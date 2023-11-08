4cc252a82db1d9a8a27d91f908d9ab407cc9386d
ukarsh2004git <utkarshshahare2004@gmail.com>
2023-11-08 10:56:19 +0530

    Stack and push

--- resources/views/layout1.blade.php
+++ resources/views/layout1.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.main')
-
+@push('title')<title>layout1</title>
 @section('main-section')
 <h1>Hello this is Layout 1 </h1>
 
--- resources/views/layout2.blade.php
+++ resources/views/layout2.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.main')
-
+@push('title')<title>layout2</title>
 @section('main-section')
     <h1>This is Layout 2</h1>
 @endsection
